950938b877b17b6a4696b7eaedb51eda69ef4f1c
kumar-utsav <kmr.utsav@gmail.com>
2016-01-09 17:12:36 -0500

    Parse

--- Sample/parser.js
+++ Sample/parser.js
@@ -1,11 +1,21 @@
 "use strict"
 
 var fs = require("fs");
+var childProcess = require('child_process');
 
 fs.readFile("package.json", "utf8", function(err, data){
 
   var obj = JSON.parse(data);
   var dependencies = obj.dependencies;
   var devDependencies = obj.devDependencies;
+
+  for(var key in dependencies){
+    var installedVersion = dependencies[key];
+    childProcess.exec('npm show ' + key + ' --json', function(error, stdout, stderr){
+      var pckgInfo = JSON.parse(stdout)
+      // console.log(pckgInfo)
+      console.log(pckgInfo.name + " : " + pckgInfo.version)
+    });
+  }
 
 })
