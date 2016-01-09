5efc1246aeacdc104354d9c099bc7bef27c66e4a
kumar-utsav <kmr.utsav@gmail.com>
2016-01-09 16:24:57 -0500

    Sample parser update

--- Sample/parser.js
+++ Sample/parser.js
@@ -2,10 +2,10 @@
 
 var fs = require("fs");
 
-fs.readFile("Sample/package.json", "utf8", function(err, data){
+fs.readFile("package.json", "utf8", function(err, data){
 
   var obj = JSON.parse(data);
   var dependencies = obj.dependencies;
   var devDependencies = obj.devDependencies;
-  
+
 })
