3c66b7f5c50083d42bd8b32d2adae2277cd114e7
kumar-utsav <kmr.utsav@gmail.com>
2016-01-15 10:45:41 -0500

    add async functionality

---
+++ parser.js
@@ -0,0 +1,65 @@
+"use strict"
+
+var fs = require("fs");
+var childProcess = require('child_process');
+var async = require("async");
+
+fs.readFile("package.json", "utf8", function(err, data) {
+
+    var obj = JSON.parse(data);
+    var dependencies = obj.dependencies;
+    var devDependencies = obj.devDependencies;
+    var keys = Object.keys(dependencies);
+
+    var out = [];
+
+    // console.log(dependencies);
+
+    // for (var key in dependencies) {
+    //   var installedVersion = dependencies[key];
+    //   childProcess.exec('npm show ' + key + ' --json', function(error, stdout, stderr) {
+    //     var pckgInfo = JSON.parse(stdout)
+    //       // console.log(pckgInfo)
+    //     console.log(pckgInfo.name + " : " + pckgInfo.versions)
+    //   });
+    // }
+
+    // function loop(i) {
+    //     if (i == keys.length) {
+    //         console.log(out);
+    //     } else {
+    //
+    //         var installedVersion = dependencies[keys[i]];
+    //
+    //         childProcess.exec('npm show ' + keys[i] + ' --json', function(error, stdout, stderr) {
+    //             var pckgInfo = JSON.parse(stdout);
+    //             // console.log(pckgInfo)
+    //             out.push({
+    //                 "name": pckgInfo.name,
+    //                 "versions": pckgInfo.versions
+    //             });
+    //             loop(i + 1)
+    //         });
+    //     }
+    // }
+    // loop(0);
+
+    async.each(keys, function(key, callback) {
+        childProcess.exec('npm show ' + key + ' --json', function(error, stdout, stderr) {
+            var pckgInfo = JSON.parse(stdout);
+            // console.log(pckgInfo);
+            out.push({
+                "name": pckgInfo.name,
+                "versions": pckgInfo.versions
+            });
+            callback();
+        })
+    }, function(err) {
+        if (err) {
+            console.log("Error:" + err);
+        } else {
+            console.log("Output: " + JSON.stringify(out));
+        }
+    });
+
+})
