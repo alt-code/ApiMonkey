72b8a217ac560c427c3287147ebcd77af847e8d7
kumar-utsav <kmr.utsav@gmail.com>
2016-01-16 14:51:11 -0500

    code mods

--- parser.js
+++ parser.js
@@ -11,7 +11,7 @@ fs.readFile("package.json", "utf8", function(err, data) {
     var devDependencies = obj.devDependencies;
     var keys = Object.keys(dependencies);
 
-    var out = [];
+    var outdated = [];
 
     // console.log(dependencies);
 
@@ -45,20 +45,26 @@ fs.readFile("package.json", "utf8", function(err, data) {
     // loop(0);
 
     async.each(keys, function(key, callback) {
+
         childProcess.exec('npm show ' + key + ' --json', function(error, stdout, stderr) {
+
             var pckgInfo = JSON.parse(stdout);
-            // console.log(pckgInfo);
-            out.push({
-                "name": pckgInfo.name,
-                "versions": pckgInfo.versions
-            });
+            var currentVersion = dependencies[key].replace(/[^\d.]/g, '');
+
+            if(currentVersion !== pckgInfo.version){
+                outdated.push({
+                    "name": pckgInfo.name,
+                    "versions": pckgInfo.versions,
+                    "installedVersion": currentVersion
+                });
+            }
             callback();
         })
     }, function(err) {
         if (err) {
             console.log("Error:" + err);
         } else {
-            console.log("Output: " + JSON.stringify(out));
+            console.log("Output: " + JSON.stringify(outdated));
         }
     });
 
