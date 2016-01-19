003f03b5dc4d1fa94d886bc6ab21696908d346b5
kumar-utsav <kmr.utsav@gmail.com>
2016-01-19 16:34:38 -0500

    Implement caching

--- parser.js
+++ parser.js
@@ -3,6 +3,17 @@
 var fs = require("fs");
 var childProcess = require('child_process');
 var async = require("async");
+var map = new Object();
+
+if (fs.existsSync('cache.json')) {
+    var cacheData = JSON.parse(fs.readFileSync('cache.json', "utf8"));
+    for (var i in cacheData) {
+        map[cacheData[i].name] = cacheData[i].versions
+    }
+} else {
+    fs.writeFileSync('cache.json', '');
+}
+
 
 fs.readFile("package.json", "utf8", function(err, data) {
 
@@ -12,59 +23,55 @@ fs.readFile("package.json", "utf8", function(err, data) {
     var keys = Object.keys(dependencies);
 
     var outdated = [];
-
-    // console.log(dependencies);
-
-    // for (var key in dependencies) {
-    //   var installedVersion = dependencies[key];
-    //   childProcess.exec('npm show ' + key + ' --json', function(error, stdout, stderr) {
-    //     var pckgInfo = JSON.parse(stdout)
-    //       // console.log(pckgInfo)
-    //     console.log(pckgInfo.name + " : " + pckgInfo.versions)
-    //   });
-    // }
-
-    // function loop(i) {
-    //     if (i == keys.length) {
-    //         console.log(out);
-    //     } else {
-    //
-    //         var installedVersion = dependencies[keys[i]];
-    //
-    //         childProcess.exec('npm show ' + keys[i] + ' --json', function(error, stdout, stderr) {
-    //             var pckgInfo = JSON.parse(stdout);
-    //             // console.log(pckgInfo)
-    //             out.push({
-    //                 "name": pckgInfo.name,
-    //                 "versions": pckgInfo.versions
-    //             });
-    //             loop(i + 1)
-    //         });
-    //     }
-    // }
-    // loop(0);
+    var allDependencies = [];
 
     async.each(keys, function(key, callback) {
 
-        childProcess.exec('npm show ' + key + ' --json', function(error, stdout, stderr) {
+        var currentVersion = dependencies[key].replace(/[^\d.]/g, '');
 
-            var pckgInfo = JSON.parse(stdout);
-            var currentVersion = dependencies[key].replace(/[^\d.]/g, '');
+        if(key in map){
 
-            if(currentVersion !== pckgInfo.version){
+            var versions = map[key];
+
+            allDependencies.push({
+                "name": key,
+                "versions": versions
+            });
+
+            if (currentVersion != versions[versions.length - 1]) {
                 outdated.push({
-                    "name": pckgInfo.name,
-                    "versions": pckgInfo.versions,
-                    "installedVersion": currentVersion
+                    "name": key,
+                    "versions": versions
                 });
             }
             callback();
-        })
+
+        }else{
+
+            childProcess.exec('npm show ' + key + ' --json', function(error, stdout, stderr) {
+
+                var pckgInfo = JSON.parse(stdout);
+
+                allDependencies.push({
+                    "name": pckgInfo.name,
+                    "versions": pckgInfo.versions
+                });
+
+                if (currentVersion != pckgInfo.version) {
+                    outdated.push({
+                        "name": pckgInfo.name,
+                        "versions": pckgInfo.versions
+                    });
+                }
+                callback();
+            })
+        }
     }, function(err) {
         if (err) {
-            console.log("Error:" + err);
+            console.log("Error: " + err);
         } else {
-            console.log("Output: " + JSON.stringify(outdated));
+            console.log("Ouput: " + JSON.stringify(outdated));
+            fs.writeFileSync('cache.json', JSON.stringify(allDependencies));
         }
     });
 
