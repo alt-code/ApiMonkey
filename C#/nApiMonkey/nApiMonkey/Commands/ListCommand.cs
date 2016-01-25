888fccc5fe5213b3c8f3ce955c63831730295859
Chris Parnin <chris.parnin@gmail.com>
2016-01-24 19:25:19 -0500

    initial version of C# driver

---
+++ C#/nApiMonkey/nApiMonkey/Commands/ListCommand.cs
@@ -0,0 +1,18 @@
+﻿using NuGet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nApiMonkey.Commands
+{
+    class ListCommand
+    {
+        public IList<IPackage> Execute(string packageId)
+        {
+            IPackageRepository repo = PackageRepositoryFactory.Default.CreateRepository("https://packages.nuget.org/api/v2");
+            return repo.FindPackagesById(packageId).ToList();
+        }
+    }
+}
---
+++ C#/nApiMonkey/nApiMonkey/Program.cs
@@ -0,0 +1,26 @@
+﻿using nApiMonkey.Commands;
+using NuGet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nApiMonkey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListCommand listCmd = new ListCommand();
+            var results = listCmd.Execute("nunit");
+            foreach( var package in results )
+            {
+                Console.WriteLine(package.Id + package.Version);
+            }
+
+            // Waits for any key to be pressed.
+            Console.ReadKey();
+        }
+    }
+}
