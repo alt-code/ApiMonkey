6462252e3bca3db325ebf090b414dc267dc244a7
Kumar Utsav <utsav@Kumars-MacBook-Pro.local>
2016-02-21 12:30:17 -0500

    Complete basic project requirements

--- js/app.js
+++ js/app.js
@@ -6,86 +6,105 @@ var utilities = require('./utilities.js')
 var repo = process.argv[2];
 var projectName = process.argv[3];
 
-utilities.initialSetup(repo, projectName, function (config) {
+utilities.initialSetup(repo, projectName, function(config) {
+    console.log('Initial setup done.');
 
-    utilities.parsePackageJSON(projectName, function (obj) {
+    utilities.parsePackageJSON(projectName, function(obj) {
+        console.log('Parse package done.');
 
         var dependencies = obj.dependencies;
         var devDependencies = obj.devDependencies;
         var keys = Object.keys(dependencies);
         var testCmd = utilities.modifyTestCmd(obj)
 
-        utilities.checkForCache(projectName, function (map) {
+        utilities.runInitialTests(projectName, testCmd, function(output) {
+            console.log('Initial tests done.');
 
-            var outdated = [];
-            var allDependencies = [];
+            utilities.getNumbers(output, function(initialTestsResult) {
+                console.log('Initial tests results: ' + JSON.stringify(initialTestsResult));
 
-            async.each(keys, function(key, callback) {
+                utilities.checkForCache(projectName, function(map) {
+                    console.log('Check for cache done.');
 
-                var currentVersion = dependencies[key].replace(/[^\d.]/g, '');
+                    var outdated = [];
+                    var allDependencies = [];
 
-                if (key in map) {
-                    var versions = map[key].versions;
+                    async.each(keys, function(key, callback) {
 
-                    allDependencies = utilities.addToArray(allDependencies, key, versions);
+                        var currentVersion = dependencies[key].replace(/[^\d.]/g, '');
 
-                    if (currentVersion != versions[versions.length - 1]) {
-                        var newerVersions = utilities.getNewerVersions(versions, currentVersion);
-                        outdated = utilities.addToArray(outdated, key, newerVersions);
-                    }
+                        if (key in map) {
+                            var versions = map[key].versions;
 
-                    callback();
+                            allDependencies = utilities.addToArray(allDependencies, key, versions);
 
-                } else {
+                            if (currentVersion != versions[versions.length - 1]) {
+                                var newerVersions = utilities.getNewerVersions(versions, currentVersion);
+                                outdated = utilities.addToArray(outdated, key, newerVersions);
+                            }
 
-                    childProcess.exec('npm show ' + key + ' --json', function(error, stdout, stderr) {
+                            callback();
 
-                        var pckgInfo = JSON.parse(stdout);
-
-                        allDependencies = utilities.addToArray(allDependencies, pckgInfo.name, pckgInfo.versions);
-
-                        if (currentVersion != pckgInfo.version) {
-                            var newerVersions = utilities.getNewerVersions(pckgInfo.versions, currentVersion);
-                            outdated = utilities.addToArray(outdated, pckgInfo.name, newerVersions);
-                        }
-
-                        callback();
-                    })
-                }
-            }, function(err) {
-                if (err) {
-                    console.log("Error: " + err);
-                } else {
-
-                    fs.writeFileSync(projectName + '/cache.json', JSON.stringify(allDependencies));
-
-                    loop(0);
-
-                    function loop(i) {
-                        if (i == outdated.length) {
-                            console.log("All done.");
                         } else {
-                            var depName = outdated[i].name;
-                            var depVersions = outdated[i].versions;
-                            async.each(depVersions, function(depVersion, callback) {
 
-                                var verPath =  projectName + '/dependencies/' + depName + '/' + depVersion
-                                var tasks = 'git clone ' + repo + ' ' + verPath + ' && cd ' + verPath + ' && npm install && npm install ' +  depName + '@' + depVersion;
-                                childProcess.exec(tasks, function(error, stdout, stderr) {
-                                    childProcess.exec('cd ' + verPath + ' && ' + testCmd, config, function(error, stdout, stderr){
-                                        console.log(stdout + "\n" + depName + '/' + depVersion + ' done.\n');
-                                        callback();
-                                    })
-                                })
+                            childProcess.exec('npm show ' + key + ' --json', function(error, stdout, stderr) {
 
-                            }, function(err) {
-                                console.log(depName + ' done.');
-                                loop(i + 1);
+                                var pckgInfo = JSON.parse(stdout);
+
+                                allDependencies = utilities.addToArray(allDependencies, pckgInfo.name, pckgInfo.versions);
+
+                                if (currentVersion != pckgInfo.version) {
+                                    var newerVersions = utilities.getNewerVersions(pckgInfo.versions, currentVersion);
+                                    outdated = utilities.addToArray(outdated, pckgInfo.name, newerVersions);
+                                }
+
+                                callback();
                             })
                         }
-                    }
-                }
-            });
+                    }, function(err) {
+                        if (err) {
+                            console.log("Error: " + err);
+                        } else {
+
+                            fs.writeFileSync(projectName + '/cache.json', JSON.stringify(allDependencies));
+                            console.log('Outdated dependencies: ' + JSON.stringify(outdated));
+
+                            loop(0);
+
+                            function loop(i) {
+                                if (i == outdated.length) {
+                                    console.log("All done.");
+                                } else {
+                                    var depName = outdated[i].name;
+                                    var depVersions = outdated[i].versions;
+                                    async.each(depVersions, function(depVersion, callback) {
+
+                                        var verPath = projectName + '/dependencies/' + depName + '/' + depVersion
+                                        var tasks = 'git clone ' + repo + ' ' + verPath + ' && cd ' + verPath + ' && npm install && npm install ' + depName + '@' + depVersion;
+                                        childProcess.exec(tasks, function(error, stdout, stderr) {
+                                            childProcess.exec('cd ' + verPath + ' && ' + testCmd, config, function(error, stdout, stderr) {
+                                                utilities.getNumbers(stdout, function(newTestsResult) {
+                                                    console.log("Test results with " + depName + '@' + depVersion + ': ' + JSON.stringify(newTestsResult));
+                                                    if (newTestsResult.tests != initialTestsResult.tests || newTestsResult.failures != initialTestsResult.failures) {
+                                                        fs.appendFileSync('report.md', depName + '@' + depVersion + ': Failed' + '\n')
+                                                    } else {
+                                                        fs.appendFileSync('report.md', depName + '@' + depVersion + ': Passed' + '\n')
+                                                    }
+                                                    console.log(depName + '@' + depVersion + ' done.');
+                                                    callback();
+                                                })
+                                            })
+                                        })
+                                    }, function(err) {
+                                        console.log(depName + ' done.');
+                                        loop(i + 1);
+                                    })
+                                }
+                            }
+                        }
+                    });
+                })
+            })
         })
     })
 })
--- js/utilities.js
+++ js/utilities.js
@@ -1,5 +1,6 @@
 var fs = require('fs')
 var childProcess = require('child_process')
+var parser = require('xml2json');
 
 // https://maymay.net/blog/2008/06/15/ridiculously-simple-javascript-version-string-to-object-parser/
 function parseVersionString(str) {
@@ -22,13 +23,13 @@ var cmdMap = {};
 
 // multiple test suites can be added here
 
-cmdMap['mocha'] = function (cmd) {
+cmdMap['mocha'] = function(cmd) {
     return cmd.replace(/mocha/g, 'mocha -R xunit')
 }
 
 module.exports = {
 
-    getNewerVersions: function (versionArray, installedVersion) {
+    getNewerVersions: function(versionArray, installedVersion) {
         var newerVersions = [];
         for (var i = 0; i < versionArray.length; i++) {
             var ver = parseVersionString(versionArray[i]);
@@ -38,7 +39,7 @@ module.exports = {
             } else if (curVer.major == ver.major) {
                 if (parseFloat(curVer.minor) < parseFloat(ver.minor)) {
                     newerVersions.push(versionArray[i]);
-                }else if(curVer.minor == ver.minor){
+                } else if (curVer.minor == ver.minor) {
                     if (parseFloat(curVer.patch) < parseFloat(ver.patch)) {
                         newerVersions.push(versionArray[i]);
                     }
@@ -48,25 +49,26 @@ module.exports = {
         return newerVersions;
     },
 
-    modifyTestCmd: function (obj) {
+    modifyTestCmd: function(obj) {
         var testCmd = obj.scripts['test']
-        if(testCmd.indexOf('mocha') != 0){
+        if (testCmd.indexOf('mocha') != 0) {
             return cmdMap['mocha'](testCmd)
         }
     },
 
-    initialSetup: function (url, projectName, callback) {
+    initialSetup: function(url, projectName, callback) {
+
         var env = process.env
         env.PATH = env.PATH + ':node_modules/.bin'
         var config = {
             env: env
         }
-        childProcess.exec('git clone ' + url + ' ' + projectName + ' && cd ' + projectName + ' && npm install', function(){
+        childProcess.exec('touch report.md && git clone ' + url + ' ' + projectName + ' && cd ' + projectName + ' && npm install', function(err, stdout, stderr) {
             callback(config)
         })
     },
 
-    addToArray: function (array, key, versions, url) {
+    addToArray: function(array, key, versions, url) {
         array.push({
             "name": key,
             "versions": versions
@@ -74,7 +76,7 @@ module.exports = {
         return array
     },
 
-    checkForCache: function (path, callback) {
+    checkForCache: function(path, callback) {
         var map = new Object();
         if (fs.existsSync(path + '/cache.json')) {
             var cacheData = JSON.parse(fs.readFileSync(path + '/cache.json', "utf8"));
@@ -90,10 +92,33 @@ module.exports = {
         callback(map)
     },
 
-    parsePackageJSON: function (path, callback) {
+    parsePackageJSON: function(path, callback) {
         fs.readFile(path + '/package.json', "utf8", function(err, data) {
             var obj = JSON.parse(data);
             callback(obj)
         })
+    },
+
+    runInitialTests: function(projectName, testCmd, callback) {
+        childProcess.exec('cd ' + projectName + ' && ' + testCmd, function(err, stdout, stderr) {
+            callback(stdout)
+        })
+    },
+
+    getNumbers: function(testResults, callback) {
+        var result = {
+            tests: 0,
+            failures: 0
+        }
+        var lines = testResults.split('\n')
+        for (var i = 0; i < lines.length; i++) {
+            var line = lines[i]
+            if (line.indexOf('<testsuite') != -1) {
+                var json = JSON.parse(parser.toJson(line))
+                result.tests += parseInt(json.testsuite.tests)
+                result.failures += parseInt(json.testsuite.failures)
+            }
+        }
+        callback(result);
     }
 }
