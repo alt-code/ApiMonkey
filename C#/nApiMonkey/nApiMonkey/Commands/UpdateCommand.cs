58bf22648932de7a8608fd8be9ce8ef4985d8741
tanvi <tanvi.mainkar@gmail.com>
2016-04-18 16:06:54 -0400

    fixing update of multiple projects referring to same dependency

--- C#/nApiMonkey/nApiMonkey/Commands/UpdateCommand.cs
+++ C#/nApiMonkey/nApiMonkey/Commands/UpdateCommand.cs
@@ -4,11 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NuGet;
-//using Microsoft.Web.Infrastructure;
-//using System.Web.WebPages;
-//using Microsoft.VisualStudio;
-//using MsBuildProject = Microsoft.Build.Evaluation.Project;
-//using Project1 = EnvDTE.Project;
 using NuGet.VisualStudio;
 using System.IO;
 using NuGet.Common;
@@ -21,23 +16,27 @@ namespace nApiMonkey
         {
             try
             {
+                System.Console.WriteLine("package "+packageid);
+                System.Console.WriteLine("csporj " + projectFile);
+                System.Console.WriteLine("version "+version);
+                System.Console.WriteLine("packages "+packages);
                 IPackageRepository repo = PackageRepositoryFactory.Default.CreateRepository("https://packages.nuget.org/api/v2");
                 var packagePathResolver = new DefaultPackagePathResolver(packages);
                 var packagesFolderFileSystem = new PhysicalFileSystem(packages);
                 var projectSystem = new MSBuildProjectSystem(projectFile);
                 var localRepository = new LocalPackageRepository(packagePathResolver, packagesFolderFileSystem);
                 var projectManager = new ProjectManager(repo, packagePathResolver, projectSystem, localRepository);
-
-                projectManager.UpdatePackageReference(packageid, version, false, false);
+                projectManager.RemovePackageReference(packageid);
+                projectManager.AddPackageReference(packageid, version, true, false);
                 projectSystem.Save();
 
-                System.Console.WriteLine("Updating "+packages);
+               // System.Console.WriteLine("Updating "+packages);
                 string filename = packageid + "." + version;
                 string[] s = Directory.GetFiles(packages+ @"\"+filename);
                 if (s.IsEmpty()) { System.Console.WriteLine("empty"); }
                 else
                 {
-                    System.Console.WriteLine("nupkg file: " + s.First());
+                 //   System.Console.WriteLine("nupkg file: " + s.First());
                     var nupkgFile = new PhysicalFileSystem(s[0]);
                     ZipPackage z = new ZipPackage(s[0]);
                     z.ExtractContents(nupkgFile, packages + @"\" + filename);
